56c5ea07db783de5e071a88e19b9a71b00cc6301
soni17 <elb004@gmail.com>
2017-10-11 18:37:07 -0400

    added more routes

--- crm.rb
+++ crm.rb
@@ -4,3 +4,17 @@ require 'sinatra'
 get '/' do
   erb :index
 end
+
+get '/contacts' do
+  @allContacts = Contact.all()
+  erb :contacts
+end
+
+get '/about' do
+  erb :about
+end
+
+
+after do
+  ActiveRecord::Base.connection.close
+end
